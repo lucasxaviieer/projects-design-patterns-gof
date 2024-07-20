5fdcf6a62a4d1f009e257948ed8dcd3df1b96f70
Lucas Xavier <lucas.lima.xaviier@gmail.com>
2024-07-19 20:59:39 -0400

    PutMapping e DeleteMapping adicionados ao Controller de Cliente

--- padroes-projetos-spring2/src/main/java/lucas/gof/controller/ClienteController.java
+++ padroes-projetos-spring2/src/main/java/lucas/gof/controller/ClienteController.java
@@ -2,9 +2,11 @@ package lucas.gof.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,27 +17,35 @@ import lucas.gof.service.ClienteService;
 @RestController
 @RequestMapping("clientes")
 public class ClienteController {
-	
+
 	@Autowired
-	private ClienteService service; 
-	
+	private ClienteService service;
+
 	@GetMapping
-	public ResponseEntity<Iterable<Cliente>> buscarTodos(){
+	public ResponseEntity<Iterable<Cliente>> buscarTodos() {
 		return ResponseEntity.ok(service.buscarTodos());
 	}
-	
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id){
+	public ResponseEntity<Cliente> buscarPorId(@PathVariable Long id) {
 		return ResponseEntity.ok(service.buscarPorId(id));
 	}
-	
+
 	@PostMapping
 	public ResponseEntity<Cliente> inserir(@RequestBody Cliente cliente) {
 		service.inserir(cliente);
 		return ResponseEntity.ok(cliente);
 	}
-	
-	
 
+	@PutMapping("/{id}")
+	public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+		service.atualizar(id, cliente);
+		return ResponseEntity.ok(cliente);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Cliente> excluir(@PathVariable Long id) {
+		service.excluir(id);
+		return ResponseEntity.ok().build();
+	}
 }
